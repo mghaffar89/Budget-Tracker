a0d6cb14b29b510f45fb74d3b4131ecd21b4c999
Mina G <mghaffar89@gmail.com>
2020-08-20 01:53:15 -0500

    added error function, post function and getting all the data

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -18,3 +18,37 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
   console.error(event.target.errorCode);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(["transaction"], "readwrite");
+  const store = transaction.objectStore("transaction");
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["transaction"], "readwrite");
+  const store = transaction.objectStore("transaction");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["transaction"], "readwrite");
+          const store = transaction.objectStore("transaction");
+
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
