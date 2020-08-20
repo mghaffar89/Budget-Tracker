dd8d4727965a44452b688928f8cda4d5f5c80429
Mina G <mghaffar89@gmail.com>
2020-08-20 01:07:17 -0500

    created index db file which will include all online and offline functions

---
+++ public/indexedDb.js
@@ -0,0 +1,20 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("transaction", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.error(event.target.errorCode);
+};
